14cd97b1e35df6716694ff78ff6f3abe193f07e4
Reshma <reshma@omtecweb.com>
2023-03-09 13:29:12 +0530

    add image file

---
+++ dater/lib/constants/api_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const rootPath = "assets/images";
+
+  static const splashScreenImage = "$rootPath/splash_image.png";
+}
--- dater/lib/screens/splash_screen/splash_screen.dart
+++ dater/lib/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -6,6 +7,15 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: Get.width,
+            height: Get.height,
+            decoration: BoxDecoration(image: DecorationImage(image: AssetImage(""))),
+          ),
+        ],
+      ),
     );
   }
 }
