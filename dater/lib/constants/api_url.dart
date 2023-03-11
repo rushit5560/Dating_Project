a21150a31f63fce1615954cdb5e2650a8e82d753
Reshma <reshma@omtecweb.com>
2023-03-11 12:46:13 +0530

    yaml

--- dater/lib/constants/api_url.dart
+++ dater/lib/constants/api_url.dart
@@ -1,1 +1,9 @@
-class ApiUrl {}
+class ApiUrl {
+  static const apiMainPath = "https://oudeen.com/dater/";
+  //authantication api's
+  static const getGenderApi = "${apiMainPath}api/get_genders";
+  static const getGoalApi = "${apiMainPath}api/get_goals";
+
+  static const getInterestsApi = "${apiMainPath}api/get_interests";
+  static const saveInterestsApi = "${apiMainPath}api/set_interest";
+}
