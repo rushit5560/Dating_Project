de24c8aa9a4bcb8629364d0b364359907275784a
Reshma <reshma@omtecweb.com>
2023-03-14 09:51:00 +0530

    controller changes

--- dater/lib/controller/auth_screen_controllers/verification_code_screen_controller.dart
+++ dater/lib/controller/auth_screen_controllers/verification_code_screen_controller.dart
@@ -1,8 +1,15 @@
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:dater/constants/api_url.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class VerifyCodeScreenController extends GetxController {
-  String mobileNumber = Get.arguments[0] ?? "";
+  String countryCode = Get.arguments[0] ?? "";
+  String mobileNumber = Get.arguments[1] ?? "";
+
+  RxBool isLoading = false.obs;
+
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   TextEditingController firstDigitController = TextEditingController();
@@ -27,4 +34,33 @@ class VerifyCodeScreenController extends GetxController {
     "0",
     "#",
   ];
+
+
+
+  Future<void> activateAccountFunction() async {
+    isLoading(true);
+    String url = ApiUrl.accountActiveApi;
+    log('activateAccountFunction Api Url : $url');
+
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(url));
+
+      request.fields['phone'] = mobileNumber;
+      request.fields['verify_token'] = mobileNumber;
+
+
+    } catch(e) {
+      log('activateAccountFunction Error :$e');
+      rethrow;
+    }
+  }
+
+
+  @override
+  void onInit() {
+    log('countryCode : $countryCode');
+    log('mobileNumber : $mobileNumber');
+    super.onInit();
+  }
+ 
 }
