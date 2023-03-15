f559839c4e581244ea95eeb4206e3fccea745156
Reshma <reshma@omtecweb.com>
2023-03-15 14:07:25 +0530

    balance screen

---
+++ dater/lib/screens/balance_screen/balance_screen.dart
@@ -0,0 +1,16 @@
+import 'package:dater/common_modules/bottom_navigation_bar.dart';
+import 'package:dater/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+
+class BalanceScreen extends StatelessWidget {
+  const BalanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.whiteColor2,
+      //  bottomNavigationBar:  BottomNavigationBarModule(),
+    );
+  }
+}
